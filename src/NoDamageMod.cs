06f599f1ee6e1dba7404896cb3f6fbabd5946e8c
Franc1sco Franug <franug13@gmail.com>
2024-01-03 23:18:38 +0100

    version 1.1
    Added command for enable/disable and config for set command access

--- src/NoDamageMod.cs
+++ src/NoDamageMod.cs
@@ -1,21 +1,34 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using System.Text.Json.Serialization;
 
 namespace NoDamageMod;
 
+public class ConfigGen : BasePluginConfig
+{
+    [JsonPropertyName("EnabledDamageByDefault")] public bool Enabled { get; set; } = false;
+    [JsonPropertyName("CommandAccessFlag")] public string CommandAccessFlag { get; set; } = "@css/vip";
+}
 
 [MinimumApiVersion(142)]
-public class NoDamageMod : BasePlugin
+public class NoDamageMod : BasePlugin, IPluginConfig<ConfigGen>
 {
     public override string ModuleName => "No Damage Mod";
     public override string ModuleAuthor => "Franc1sco Franug";
-    public override string ModuleVersion => "1.0";
+    public override string ModuleVersion => "1.1";
+    public ConfigGen Config { get; set; } = null!;
+    public void OnConfigParsed(ConfigGen config) { Config = config; }
+    private bool bDamageEnabled;
 
     public override void Load(bool hotReload)
     {
+        bDamageEnabled = Config.Enabled;
         VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Hook(OnTakeDamage, HookMode.Pre);
     }
 
@@ -24,11 +37,31 @@ public class NoDamageMod : BasePlugin
         VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Unhook(OnTakeDamage, HookMode.Pre);
     }
 
+    [ConsoleCommand("css_damage", "")]
+    public void CommandDamage(CCSPlayerController? player, CommandInfo info)
+    {
+        if (player == null || !player.IsValid)
+        {
+            return;
+        }
+
+        if (Config.CommandAccessFlag != "" && !AdminManager.PlayerHasPermissions(player, Config.CommandAccessFlag))
+        {
+            info.ReplyToCommand("You dont have access for this command");
+            return;
+        }
+
+        bDamageEnabled = !bDamageEnabled;
+
+        if (bDamageEnabled) info.ReplyToCommand("Damage Enabled");
+        else info.ReplyToCommand("Damage Disabled");
+    }
+
     HookResult OnTakeDamage(DynamicHook handle)
     {
         CEntityInstance victim = handle.GetParam<CEntityInstance>(0);
 
-        if (victim != null)
+        if (victim != null && !bDamageEnabled)
         {
             return HookResult.Handled;
         }
